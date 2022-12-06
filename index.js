37b5e51fe1b75660c86e39ea5fd6605aec8e5de6
derickboomer <rhoderick.rodriguez.cics@ust.edu.ph>
2022-12-06 13:45:37 +0800

    make it better

--- index.js
+++ index.js
@@ -751,7 +751,7 @@ app.post("/filter", (req, res) => {
 // PORT
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-  console.log("Lisenting on port " + port + "...");
+  console.log("Listening on port " + port + "...");
 });
 
 // app.listen(process.env.PORT || PORT, () =>{
